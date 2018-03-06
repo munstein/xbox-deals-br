01ca06ea4ef73a6eebcacba627df28d56f10442c
munstein <eduardo.munstein@live.com>
2018-03-06 16:11:51 -0300

    Refactoring

--- app/src/test/java/com/munstein/xboxdealsbr/PresenterTest.kt
+++ app/src/test/java/com/munstein/xboxdealsbr/PresenterTest.kt
@@ -1,8 +1,10 @@
 package com.munstein.xboxdealsbr
 
 import com.munstein.xboxdealsbr.modules.main.MainMVP
+import org.junit.Before
 import org.junit.Rule
 import org.mockito.Mock
+import org.mockito.Mockito.mock
 import org.mockito.junit.MockitoJUnit
 
 
@@ -19,5 +21,11 @@ class PresenterTest{
 
     @Rule
     var mockitoRule = MockitoJUnit.rule()
+
+    @Before
+    fun setup(){
+        modelMock = mock(MainMVP.model::class.java)
+        viewMock = mock(MainMVP.view::class.java)
+    }
 
 }
