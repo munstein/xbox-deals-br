eb26b38cedbf5c1d3c8d55f97877470f6d7bc3f4
munstein <eduardo.munstein@live.com>
2018-04-17 22:51:41 -0300

    Add deals from special promotions

--- app/src/main/java/com/munstein/xboxdealsbr/core/DealsMachineJsoup.kt
+++ app/src/main/java/com/munstein/xboxdealsbr/core/DealsMachineJsoup.kt
@@ -68,17 +68,21 @@ open class DealsMachineJsoup : IDealsMachine {
 
     private fun getDealsFromTables(tables: Elements): List<Deal> {
         if (tables.size > 0) {
-            var tableContents = tables[0].select("tr")
             var deals = ArrayList<Deal>()
-            for (i in 2..tableContents.size - 1) {
-                var tds = tableContents[i].select("td")
-                var game = tds[0].text()
-                var type = tds[1].text()
-                var url = tds[0].select("a").attr("href")
-                var discount = tds[2].text()
-                var value = tds[3].text()
-                deals.add(Deal(game, type, discount, value, url))
+            for(i in 0 .. tables.size - 1) {
+                var tableContents = tables[i].select("tr")
+                if(tableContents.select("td")[0].text().contains("Xbox One"))
+                for (x in 2..tableContents.size - 1) {
+                    var tds = tableContents[x].select("td")
+                    var game = tds[0].text()
+                    var type = tds[1].text()
+                    var url = tds[0].select("a").attr("href")
+                    var discount = tds[2].text()
+                    var value = tds[3].text()
+                    deals.add(Deal(game, type, discount, value, url))
+                }
             }
+            deals.sortBy {it.game}
             return deals
         } else {
             return ArrayList<Deal>()
