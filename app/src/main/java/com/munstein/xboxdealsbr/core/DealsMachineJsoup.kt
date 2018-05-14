342b26b618df29e8a10b32087e3decedae1893e1
munstein <eduardo.munstein@live.com>
2018-05-13 21:50:19 -0300

    Minor refactor.

--- app/src/main/java/com/munstein/xboxdealsbr/core/DealsMachineJsoup.kt
+++ app/src/main/java/com/munstein/xboxdealsbr/core/DealsMachineJsoup.kt
@@ -13,30 +13,30 @@ open class DealsMachineJsoup : IDealsMachine {
 
     override fun getLatestDealsFromHTML(html: String): List<Deal> {
         if (html.length > 0) {
-            var doc = Jsoup.parse(html)
-            var url = getLatestDealsURL(doc)
-            var elements = getDealsTables(url)
+            val doc = Jsoup.parse(html)
+            val url = getLatestDealsURL(doc)
+            val elements = getDealsTables(url)
             return getDealsFromTables(elements)
         } else {
-            return ArrayList<Deal>()
+            return ArrayList()
         }
     }
 
     override fun getLatestDealsFromURL(baseUrl: String): List<Deal> {
         try {
-            var doc = Jsoup.connect(baseUrl).get()
-            var url = getLatestDealsURL(doc)
-            var elements = getDealsTables(url)
+            val doc = Jsoup.connect(baseUrl).get()
+            val url = getLatestDealsURL(doc)
+            val elements = getDealsTables(url)
             return getDealsFromTables(elements)
         } catch (exception: Exception) {
-            return ArrayList<Deal>()
+            return ArrayList()
         }
     }
 
     override fun getTitle(html: String): String {
         try {
-            var doc = Jsoup.parse(html)
-            var title = doc.select(".post-title").filter({
+            val doc = Jsoup.parse(html)
+            val title = doc.select(".post-title").filter({
                 it.text().contains("with Gold")
             }).first()
             return title.text()
@@ -47,10 +47,10 @@ open class DealsMachineJsoup : IDealsMachine {
 
     private fun getLatestDealsURL(doc: Document): String {
         try {
-            var elements = doc.select("article")
-            var h2 = elements.get(0).select(".title")
-            var a = h2.get(0).select("a")
-            var url = a.attr("href")
+            val elements = doc.select("article")
+            val h2 = elements.get(0).select(".title")
+            val a = h2.get(0).select("a")
+            val url = a.attr("href")
             return url
         } catch (exception: Exception) {
             return ""
@@ -59,9 +59,9 @@ open class DealsMachineJsoup : IDealsMachine {
 
     private fun getDealsTables(url: String): Elements {
         if (url.length > 0) {
-            var dealsPageDocument = Jsoup.connect(url).get()
-            var div = dealsPageDocument.select(".entry-content")
-            var tables = div.select("table")
+            val dealsPageDocument = Jsoup.connect(url).get()
+            val div = dealsPageDocument.select(".entry-content")
+            val tables = div.select("table")
             return tables
         } else {
             return Elements()
@@ -70,26 +70,24 @@ open class DealsMachineJsoup : IDealsMachine {
 
     private fun getDealsFromTables(tables: Elements): List<Deal> {
         if (tables.size > 0) {
-            var deals = ArrayList<Deal>()
-            for(i in 0 .. tables.size - 1) {
-                var tableContents = tables[i].select("tr")
-                if(tableContents.select("td")[0].text().contains("Xbox One"))
-                for (x in 2..tableContents.size - 1) {
-                    var tds = tableContents[x].select("td")
-                    var game = tds[0].text()
-                    var type = tds[1].text()
-                    var url = tds[0].select("a").attr("href")
-                    var discount = tds[2].text()
-                    var value = tds[3].text()
-                    deals.add(Deal(game, type, discount, value, url))
-                }
+            val deals = ArrayList<Deal>()
+            for (i in 0..tables.size - 1) {
+                val tableContents = tables[i].select("tr")
+                if (tableContents.select("td")[0].text().contains("Xbox One"))
+                    for (x in 2 .. tableContents.size - 1) {
+                        val tds = tableContents[x].select("td")
+                        val game = tds[0].text()
+                        val type = tds[1].text()
+                        val url = tds[0].select("a").attr("href")
+                        val discount = tds[2].text()
+                        val value = tds[3].text()
+                        deals.add(Deal(game, type, discount, value, url))
+                    }
             }
-            deals.sortBy {it.game}
+            deals.sortBy { it.game }
             return deals
         } else {
-            return ArrayList<Deal>()
+            return ArrayList()
         }
     }
-
-
 }
