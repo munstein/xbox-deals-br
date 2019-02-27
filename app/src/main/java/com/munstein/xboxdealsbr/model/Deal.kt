e85ce0178c29c0aad68fb36d70774fd18ee2af8b
munstein <eduardo.munstein@live.com>
2019-02-27 20:59:10 -0300

    Runs ktlintFormat.

--- app/src/main/java/com/munstein/xboxdealsbr/model/Deal.kt
+++ app/src/main/java/com/munstein/xboxdealsbr/model/Deal.kt
@@ -4,9 +4,9 @@ package com.munstein.xboxdealsbr.model
  * Created by @Munstein on 17/01/2018. --23:33
  */
 data class Deal(
-        val game: String,
-        val type: String,
-        val discount: String,
-        val value: String,
-        val url: String
+    val game: String,
+    val type: String,
+    val discount: String,
+    val value: String,
+    val url: String
 )
